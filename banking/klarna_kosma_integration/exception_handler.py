fb830c367fd1d567e5d4892bb22b7a7b42e68ffb
marination <maricadsouza221197@gmail.com>
2023-06-05 20:12:16 +0530

    fix: Handle forbidden access due to plan validation & fallback message for server error

--- banking/klarna_kosma_integration/exception_handler.py
+++ banking/klarna_kosma_integration/exception_handler.py
@@ -20,6 +20,7 @@ class ExceptionHandler():
 
 		response = self.exception.response
 		self.handle_auth_error(response)
+		self.handle_authorization_error(response)
 		self.handle_txt_html_error(response)
 
 		content = response.json().get("message", {})
@@ -32,6 +33,16 @@ class ExceptionHandler():
 
 		frappe.log_error(title=_("Banking Error"), message=response.content)
 		frappe.throw(title=_("Banking Error"), msg=_("Authentication error due to invalid credentials."))
+
+	def handle_authorization_error(self, response):
+		if not response.status_code == 403:
+			return
+
+		frappe.log_error(title=_("Banking Error"), message=response.content)
+
+		content = response.json().get("message", {})
+		message = content if isinstance(content, str) else "Authorization error due to invalid access."
+		frappe.throw(title=_("Banking Error"), msg=_(message))
 
 	def handle_txt_html_error(self, response):
 		"""
@@ -48,7 +59,9 @@ class ExceptionHandler():
 
 		if not content and "exc_type" in response_data:
 			frappe.log_error(title=_("Banking Error"), message=response.content)
-			frappe.throw(title=_("Banking Error"), msg=response_data.get("exception"))
+
+			message = response_data.get("exception") or _("The server has errored. Please retry in some time.")
+			frappe.throw(title=_("Banking Error"), msg=message)
 
 	def handle_admin_error(self, content):
 		frappe.log_error(title=_("Banking Error"), message=json.dumps(content))
