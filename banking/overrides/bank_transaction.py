452bbf793faabc9e550cf6b0a4d64b2a98dc1d2f
marination <maricadsouza221197@gmail.com>
2024-06-03 17:23:52 +0200

    fix: Merge conflicts

--- banking/overrides/bank_transaction.py
+++ banking/overrides/bank_transaction.py
@@ -20,14 +20,10 @@ class CustomBankTransaction(BankTransaction):
 				frappe._("Bank Transaction {0} is already fully reconciled").format(self.name)
 			)
 
-<<<<<<< HEAD
-		added = False
 		# avoid mutating self.unallocated_amount (is set by erpnext on submit/update after submit)
 		unallocated_amount = flt(self.unallocated_amount)
-=======
 		pe_length_before = len(self.payment_entries)
 		invoices_to_bill = []
->>>>>>> 5e03cab (feat: Create single PE to reconcile multiple unpaid invoices)
 
 		for voucher in vouchers:
 			voucher_type, voucher_name = voucher["payment_doctype"], voucher["payment_name"]
@@ -37,36 +33,6 @@ class CustomBankTransaction(BankTransaction):
 			):
 				continue  # Can't add same voucher twice
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-			if not found:
-				payment_doctype, payment_name = voucher["payment_doctype"], voucher["payment_name"]
-				outstanding_amount = get_outstanding_amount(payment_doctype, payment_name)
-				allocated_by_voucher = min(unallocated_amount, outstanding_amount)
-
-				if outstanding_amount > 0:
-					# Make Payment Entry against the unpaid invoice, link PE to Bank Transaction
-					payment_name = self.make_payment_entry(
-						payment_doctype, payment_name, allocated_by_voucher
-					)
-					payment_doctype = "Payment Entry"  # Change doctype to PE
-=======
-			payment_doctype, payment_name = voucher["payment_doctype"], voucher["payment_name"]
-			outstanding_amount = self.get_outstanding_amount(payment_doctype, payment_name)
-
-			if outstanding_amount > 0:
-				# Make Payment Entry against the unpaid invoice, link PE to Bank Transaction
-				invoices_to_bill.append((payment_doctype, payment_name, outstanding_amount))
-			else:
-				self.add_to_payment_entry(payment_doctype, payment_name)
->>>>>>> 5e03cab (feat: Create single PE to reconcile multiple unpaid invoices)
-
-		# Make single PE against multiple invoices
-		if invoices_to_bill:
-			payment_name = self.make_pe_against_invoices(invoices_to_bill)
-			payment_doctype = "Payment Entry"  # Change doctype to PE
-			self.add_to_payment_entry(payment_doctype, payment_name)
-=======
 			outstanding_amount = get_outstanding_amount(voucher_type, voucher_name)
 			allocated_by_voucher = min(unallocated_amount, outstanding_amount)
 
@@ -85,12 +51,11 @@ class CustomBankTransaction(BankTransaction):
 				)
 			else:
 				self.add_to_payment_entry(voucher_type, voucher_name)
->>>>>>> 6e5f3be (fix: Create single PE for multiple unpaid invoices)
 
-				# Reduce unallocated amount
-				unallocated_amount = flt(
-					unallocated_amount - allocated_by_voucher, self.precision("unallocated_amount")
-				)
+			# Reduce unallocated amount
+			unallocated_amount = flt(
+				unallocated_amount - allocated_by_voucher, self.precision("unallocated_amount")
+			)
 
 		# Make single PE against multiple invoices
 		if invoices_to_bill:
@@ -101,13 +66,6 @@ class CustomBankTransaction(BankTransaction):
 		if len(self.payment_entries) != pe_length_before:
 			self.save()  # runs on_update_after_submit
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-	def make_payment_entry(
-		self, payment_doctype: str, payment_name: str, to_allocate: float
-	):
-=======
-=======
 	def validate_period_closing(self):
 		"""
 		Check if the Bank Transaction date is after the latest period closing date.
@@ -128,7 +86,6 @@ class CustomBankTransaction(BankTransaction):
 				).format(frappe.format(latest_period_close_date, "Date"))
 			)
 
->>>>>>> 907e35a (test: Tests for Issue #72 and fix Issue #81)
 	def add_to_payment_entry(self, payment_doctype, payment_name):
 		"""Add the payment entry to the bank transaction"""
 		pe = {
@@ -145,15 +102,10 @@ class CustomBankTransaction(BankTransaction):
 		# Check if the invoice is unpaid
 		return flt(frappe.db.get_value(payment_doctype, payment_name, "outstanding_amount"))
 
-<<<<<<< HEAD
-	def make_pe_against_invoice(self, payment_doctype, payment_name, outstanding_amount):
->>>>>>> 5e03cab (feat: Create single PE to reconcile multiple unpaid invoices)
-=======
 	def make_pe_against_invoices(self, invoices_to_bill):
 		"""Make Payment Entry against multiple invoices."""
 		self.validate_invoices_to_bill(invoices_to_bill)
 
->>>>>>> 6e5f3be (fix: Create single PE for multiple unpaid invoices)
 		bank_account = frappe.db.get_value("Bank Account", self.bank_account, "account")
 		first_invoice = invoices_to_bill[0]
 		if first_invoice[DOCTYPE] == "Expense Claim":
@@ -175,54 +127,10 @@ class CustomBankTransaction(BankTransaction):
 		payment_entry.posting_date = self.date
 		payment_entry.reference_no = self.reference_number or first_invoice[DOCNAME]
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-		payment_entry.reference_no = self.reference_number or payment_name
-		payment_entry.reference_date = self.date
-		payment_entry.submit()
-
-		return payment_entry.name
-
-<<<<<<< HEAD
-
-def get_outstanding_amount(payment_doctype, payment_name):
-	if payment_doctype not in ("Sales Invoice", "Purchase Invoice", "Expense Claim"):
-		return 0
-
-	if payment_doctype == "Expense Claim":
-		ec = frappe.get_doc(payment_doctype, payment_name)
-		return flt(
-			ec.total_sanctioned_amount - ec.total_amount_reimbursed,
-			ec.precision("total_sanctioned_amount"),
-		)
-
-	invoice = frappe.get_doc(payment_doctype, payment_name)
-	return flt(invoice.outstanding_amount, invoice.precision("outstanding_amount"))
-=======
-	def make_pe_against_invoices(self, invoices_to_bill):
-		"""Make Payment Entry against multiple invoices."""
-		bank_account = frappe.db.get_value("Bank Account", self.bank_account, "account")
-
-		payment_entry_dict = frappe._dict(
-			{
-				"company": self.company,
-				"payment_type": "Receive" if self.deposit > 0.0 else "Pay",
-				"reference_no": self.reference_number or invoices_to_bill[0][1],
-				"reference_date": self.date,
-				"party_type": "Customer" if self.deposit > 0.0 else "Supplier",
-				"paid_amount": self.unallocated_amount,
-				"received_amount": self.unallocated_amount,
-			}
-=======
-		invoices = split_invoices_based_on_payment_terms(
-			self.prepare_invoices_to_split(invoices_to_bill[1:]), self.company
->>>>>>> 6e5f3be (fix: Create single PE for multiple unpaid invoices)
-=======
 		# clear references to allocate invoices correctly with splits
 		payment_entry.references = []
 		invoices = split_invoices_based_on_payment_terms(
 			self.prepare_invoices_to_split(invoices_to_bill), self.company
->>>>>>> bd0628a (test: Reconcile BT with unpaid invoices)
 		)
 
 		to_allocate = self.unallocated_amount
@@ -237,11 +145,6 @@ def get_outstanding_amount(payment_doctype, payment_name):
 			if to_allocate <= 0:
 				break
 
-<<<<<<< HEAD
-		payment_entry.submit()
-		return payment_entry.name
->>>>>>> 5e03cab (feat: Create single PE to reconcile multiple unpaid invoices)
-=======
 		payment_entry.paid_amount = sum(
 			row.allocated_amount for row in payment_entry.references
 		)
@@ -295,9 +198,6 @@ def get_outstanding_amount(payment_doctype, payment_name) -> float:
 
 	invoice = frappe.get_doc(payment_doctype, payment_name)
 	return flt(invoice.outstanding_amount, invoice.precision("outstanding_amount"))
-<<<<<<< HEAD
->>>>>>> 6e5f3be (fix: Create single PE for multiple unpaid invoices)
-=======
 
 
 def on_update_after_submit(doc, event):
@@ -313,4 +213,3 @@ def on_update_after_submit(doc, event):
 				),
 				title=_("Over Allocation"),
 			)
->>>>>>> e98a265 (fix: Validate if the Bank Transaction is overallocated on update after submit)
