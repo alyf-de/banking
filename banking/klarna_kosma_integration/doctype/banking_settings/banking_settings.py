64916a24be6c36fcee928fc365f35f8d763f4540
barredterra <14891507+barredterra@users.noreply.github.com>
2025-01-29 17:21:38 +0100

    feat: remove kosma

--- banking/klarna_kosma_integration/doctype/banking_settings/banking_settings.py
+++ banking/klarna_kosma_integration/doctype/banking_settings/banking_settings.py
@@ -1,22 +1,15 @@
 # Copyright (c) 2022, ALYF GmbH and contributors
 # For license information, please see license.txt
-import json
+
 from requests import HTTPError
 from semantic_version import Version
-from typing import Dict, Optional, Union
+from typing import Dict
 
 import frappe
 from frappe import _
 from frappe.model.document import Document
 
 from banking.klarna_kosma_integration.admin import Admin
-from banking.klarna_kosma_integration.exception_handler import BankingError
-from banking.klarna_kosma_integration.utils import (
-	create_bank_account,
-	needs_consent,
-	update_bank_account,
-	update_bank,
-)
 
 
 class BankingSettings(Document):
@@ -45,121 +38,17 @@ class BankingSettings(Document):
 
 
 @frappe.whitelist()
-def get_client_token(
-	current_flow: str,
-	account: Optional[str] = None,
-	from_date: Optional[str] = None,
-	to_date: Optional[str] = None,
-	company: Optional[str] = None,
-) -> Dict:
-	"""
-	Returns Client Token to render XS2A App & Short Session ID to track session
-	"""
-	return Admin().get_client_token(current_flow, account, from_date, to_date, company)
-
-
-@frappe.whitelist()
-def fetch_accounts_and_bank(session_id_short: str = None, company: str = None) -> Dict:
-	"""
-	Fetch Accounts via Flow API after XS2A App interaction.
-	"""
-	return Admin().flow_accounts(session_id_short, company)
-
-
-@frappe.whitelist()
-def add_bank_account(
-	account_data: Union[str, dict], gl_account: str, company: str, bank_name: str
-) -> None:
-	if isinstance(account_data, str):
-		account_data = json.loads(account_data)
-
-	update_bank(account_data, bank_name)
-
-	existing_bank_account = frappe.db.exists("Bank Account", {"account": gl_account})
-	if existing_bank_account:
-		update_bank_account(account_data, existing_bank_account, relink=True)
-	else:
-		create_bank_account(account_data, bank_name, company, gl_account)
-
-
-@frappe.whitelist()
-def sync_transactions(account: str, session_id_short: Optional[str] = None) -> None:
-	"""
-	Enqueue transactions sync via the Consent API.
-	"""
-	bank, company = frappe.db.get_value("Bank Account", account, ["bank", "company"])
-
-	if not session_id_short and needs_consent(bank, company):  # UX
-		frappe.throw(
-			msg=_(
-				"The Consent Token has expired/is unavailable for Bank {0}. Please click on the {1} button"
-			).format(frappe.bold(bank), frappe.bold(_("Sync Bank and Accounts"))),
-			title=_("Kosma Error"),
-		)
-
-	frappe.enqueue(
-		"banking.klarna_kosma_integration.admin.sync_kosma_transactions",
-		account=account,
-		session_id_short=session_id_short,
-		now=frappe.conf.developer_mode,
-	)
-
-	frappe.msgprint(
-		_(
-			"Background Transaction Sync is in progress. Please check the Bank Transaction List for updates."
-		),
-		alert=True,
-		indicator="green",
-	)
-
-
-@frappe.whitelist()
 def sync_all_accounts_and_transactions():
 	"""
-	Refresh all Bank accounts and enqueue their transactions sync, via the Consent API.
+	Refresh all Bank accounts and enqueue their transactions sync.
 	Called via hooks.
 	"""
 	banking_settings = frappe.get_single("Banking Settings")
 	if not banking_settings.enabled:
 		return
 
-	if banking_settings.enable_klarna_kosma:
-		daily_sync_kosma()
-
 	if banking_settings.enable_ebics:
 		daily_sync_ebics()
-
-
-def daily_sync_kosma():
-	accounts_list = []
-
-	for bank, company in frappe.get_all(
-		"Bank Consent", fields=["bank", "company"], as_list=True
-	):
-		accounts = get_bank_accounts_to_sync(bank, company)
-		for account in accounts:
-			bank_account = frappe.db.exists("Bank Account", {"iban": account.get("iban")})
-			if not bank_account:
-				continue
-
-			update_bank_account(account, bank_account)
-			accounts_list.append(bank_account)
-
-		if not accounts:
-			accounts_list.extend(
-				frappe.get_all(
-					"Bank Account",
-					filters={
-						"bank": bank,
-						"company": company,
-						"kosma_account_id": ["is", "set"],
-					},
-					pluck="name",
-				)
-			)
-
-	for bank_account in accounts_list:
-		sync_transactions(account=bank_account)
 
 
 def daily_sync_ebics():
@@ -204,16 +93,6 @@ def intraday_sync_ebics():
 			ebics_user=ebics_user,
 			intraday=True,
 		)
-
-
-def get_bank_accounts_to_sync(bank: str, company: str) -> list:
-	"""
-	Get bank accounts from Kosma via the consent API.
-	"""
-	try:
-		return Admin().consent_accounts(bank, company)
-	except BankingError:
-		return []
 
 
 @frappe.whitelist()
--- banking/klarna_kosma_integration/doctype/klarna_kosma_session/test_klarna_kosma_session.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2022, ALYF GmbH and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestKlarnaKosmaSession(FrappeTestCase):
-	pass
