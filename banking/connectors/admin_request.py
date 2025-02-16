45c0b2009f9d804b0a3df094f1079a4e987de3a4
barredterra <14891507+barredterra@users.noreply.github.com>
2025-02-16 20:01:00 +0100

    refactor: admin request
    Resolves #128

--- banking/connectors/admin_request.py
+++ banking/connectors/admin_request.py
@@ -1,6 +1,5 @@
 # Copyright (c) 2023, ALYF GmbH and contributors
 # For license information, please see license.txt
-import json
 import requests
 
 
@@ -19,50 +18,36 @@ class AdminRequest:
 		self.url = url
 		self.customer_id = customer_id
 
-	@property
-	def headers(self):
-		return {"Alyf-Banking-Authorization": f"Token {self.api_token}"}
-
-	@property
-	def data(self):
-		return {
-			"ip_address": self.ip_address,
-			"user_agent": self.user_agent,
-			"customer_id": self.customer_id,
-		}
+	def request(self, method: str, endpoint: str, data: dict | None = None):
+		return requests.request(
+			method=method,
+			url=self.url + endpoint,
+			headers={
+				"Alyf-Banking-Authorization": f"Token {self.api_token}",
+				"Alyf-Customer-Id": self.customer_id,
+			},
+			json=data,
+		)
 
 	def fetch_subscription(self):
-		method = "banking_admin.api.fetch_subscription_details"
-		return requests.post(
-			url=self.url + method, headers=self.headers, data=json.dumps(self.data)
-		)
+		return self.request("GET", "banking_admin.api.fetch_subscription_details")
 
 	def get_customer_portal(self):
-		method = "banking_admin.api.get_customer_portal"
-		return requests.get(url=self.url + method)
+		return self.request("GET", "banking_admin.api.get_customer_portal")
 
 	def get_fintech_license(self):
-		method = "banking_admin.ebics_api.get_fintech_license"
-		return requests.post(
-			url=self.url + method, headers=self.headers, json=self.data.copy()
-		)
+		return self.request("GET", "banking_admin.ebics_api.get_fintech_license")
 
-	def register_ebics_user(self, host_id: str, partner_id: str, user_id: str):
-		data = self.data
-		data.update({"host_id": host_id, "partner_id": partner_id, "user_id": user_id})
-		method = "banking_admin.ebics_api.register_ebics_user"
-		return requests.post(
-			url=self.url + method,
-			headers=self.headers,
-			json=data,
-		)
+	def register_ebics_user(
+		self, host_id: str, partner_id: str, user_id: str, remove: bool = False
+	):
+		if remove:
+			endpoint = "banking_admin.ebics_api.remove_ebics_user"
+		else:
+			endpoint = "banking_admin.ebics_api.register_ebics_user"
 
-	def remove_ebics_user(self, host_id: str, partner_id: str, user_id: str):
-		data = self.data
-		data.update({"host_id": host_id, "partner_id": partner_id, "user_id": user_id})
-		method = "banking_admin.ebics_api.remove_ebics_user"
-		return requests.post(
-			url=self.url + method,
-			headers=self.headers,
-			json=data,
+		return self.request(
+			"POST",
+			endpoint,
+			{"host_id": host_id, "partner_id": partner_id, "user_id": user_id},
 		)
