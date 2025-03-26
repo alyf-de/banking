fda51fc0673a845108a8e3588ed18d04454fe3e9
barredterra <14891507+barredterra@users.noreply.github.com>
2025-03-26 12:15:21 +0100

    feat(Bank): show EBICS protocol versions
    (cherry picked from commit 68883caffa117b10b50b4b2b537db8829db59935)

---
+++ banking/custom/bank.py
@@ -0,0 +1,17 @@
+import frappe
+from frappe import _
+from banking.ebics.manager import get_protocol_versions as _get_protocol_versions
+
+
+@frappe.whitelist()
+def get_protocol_versions(bank_name: str):
+	bank = frappe.get_doc("Bank", bank_name)
+	bank.check_permission("write")
+
+	if not bank.ebics_host_id or not bank.ebics_url:
+		frappe.throw(
+			_("Bank {0} does not have EBICS Host ID or URL set").format(bank_name),
+			title=_("EBICS Configuration Missing"),
+		)
+
+	return _get_protocol_versions(bank.ebics_host_id, bank.ebics_url)
