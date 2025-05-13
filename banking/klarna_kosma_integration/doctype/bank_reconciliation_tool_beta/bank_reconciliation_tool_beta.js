aa50fe2e6346e70783df1ec8b6c3487f9655d79e
barredterra <14891507+barredterra@users.noreply.github.com>
2025-05-14 00:31:21 +0200

    fix: restrict CAMT upload options

--- banking/klarna_kosma_integration/doctype/bank_reconciliation_tool_beta/bank_reconciliation_tool_beta.js
+++ banking/klarna_kosma_integration/doctype/bank_reconciliation_tool_beta/bank_reconciliation_tool_beta.js
@@ -268,9 +268,13 @@ function show_camt_uploader(frm) {
 		docname: frm.doc.bank_account,
 		allow_toggle_private: false,
 		allow_take_photo: false,
+		allow_web_link: false,
+		allow_multiple: false,
+		disable_file_browser: true,
 		bank_account: frm.doc.bank_account,
 		restrictions: {
 			allowed_file_types: [".xml", ".XML"],
+			max_number_of_files: 1,
 		},
 	});
 
