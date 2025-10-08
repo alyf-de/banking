ba430e7de1b6f1fd274434b13aeb5caf4436df86
barredterra <14891507+barredterra@users.noreply.github.com>
2025-10-08 16:58:43 +0200

    feat(EBICS Request): "download files" button

--- banking/ebics/doctype/ebics_request/ebics_request.py
+++ banking/ebics/doctype/ebics_request/ebics_request.py
@@ -1,9 +1,44 @@
 # Copyright (c) 2025, ALYF GmbH and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class EBICSRequest(Document):
 	pass
+
+
+@frappe.whitelist()
+def download_files(name: str):
+	"""
+	Download the files from the EBICS Request as a zip file.
+
+	Args:
+		name: The name of the EBICS Request.
+	"""
+	import json
+	import zipfile
+	from io import BytesIO
+
+	from frappe import _
+
+	doc: EBICSRequest = frappe.get_doc("EBICS Request", name)
+	doc.check_permission()
+
+	try:
+		data = json.loads(doc.response)
+	except json.JSONDecodeError:
+		frappe.throw(_("No data available for download."))
+
+	zip_buffer = BytesIO()
+
+	zip_file = zipfile.ZipFile(zip_buffer, mode="w", compression=zipfile.ZIP_DEFLATED)
+	for file_name, file_data in data.items():
+		zip_file.writestr(file_name, file_data)
+
+	zip_file.close()
+
+	frappe.response["filecontent"] = zip_buffer.getvalue()
+	frappe.response["filename"] = f"{name}.zip"
+	frappe.response["type"] = "binary"
