d2cd2e125224cfb32d7a23ec1e2812fea7368120
barredterra <14891507+barredterra@users.noreply.github.com>
2025-10-10 19:32:35 +0200

    refactor: explicitly accept unused extra_params

--- banking/klarna_kosma_integration/doctype/bank_reconciliation_tool_beta/unpaid_vouchers.py
+++ banking/klarna_kosma_integration/doctype/bank_reconciliation_tool_beta/unpaid_vouchers.py
@@ -20,7 +20,12 @@ DOCTYPE, DOCNAME, AMOUNT, PARTY = 0, 1, 2, 3
 
 
 def get_payment_entries(
-	bt: "CustomBankTransaction", vouchers: list, reconcile_multi_party: bool = False, *args, **kwargs
+	bt: "CustomBankTransaction",
+	vouchers: list,
+	reconcile_multi_party: bool = False,
+	extra_params: dict | None = None,
+	*args,
+	**kwargs,
 ):
 	"""Reconcile unpaid invoices with the Bank Transaction."""
 	if any(
