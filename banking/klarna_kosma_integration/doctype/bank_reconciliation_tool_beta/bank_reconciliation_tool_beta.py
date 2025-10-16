7dcd6a7eab9541d9c3029bb61c9dcf267d487ba9
0xD0M1M0 <76812428+0xD0M1M0@users.noreply.github.com>
2025-10-16 15:35:58 +0200

    fix: linters

--- banking/klarna_kosma_integration/doctype/bank_reconciliation_tool_beta/bank_reconciliation_tool_beta.py
+++ banking/klarna_kosma_integration/doctype/bank_reconciliation_tool_beta/bank_reconciliation_tool_beta.py
@@ -1456,7 +1456,7 @@ def bank_query(doctype: str, txt: str, searchfield: str, start: int, page_len: i
 		limit_start=start,
 		limit_page_length=page_len,
 		group_by="bank",
-		order_by="bank"
+		order_by="bank",
 	)
 
 	return [(result,) for result in results]
