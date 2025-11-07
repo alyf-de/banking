e3906abbaf27b58c7ee835d3df32155d698271a1
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2025-11-07 14:33:07 +0100

    feat: re-import ebics request (#320)
    (cherry picked from commit 29351ff6a7e68e604429a8eec92cd3897acd1394)
    
    # Conflicts:
    #	banking/ebics/doctype/ebics_request/ebics_request.js
    #	banking/ebics/doctype/ebics_request/ebics_request.py
    #	banking/locale/de.po
    #	banking/locale/es.po
    #	banking/locale/fr.po
    #	banking/locale/it.po
    #	banking/locale/main.pot
    #	banking/locale/nl.po

--- banking/ebics/doctype/ebics_request/ebics_request.js
+++ banking/ebics/doctype/ebics_request/ebics_request.js
@@ -2,6 +2,82 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("EBICS Request", {
+<<<<<<< HEAD
 	// refresh: function(frm) {
 	// }
+=======
+	refresh: function (frm) {
+		if (!frm.is_new() && frm.doc.response.includes("{")) {
+			frm.add_custom_button(__("Download response files"), () => {
+				frm.trigger("download_files");
+			});
+
+			frm.add_custom_button(__("Re-Import"), () => {
+				frm.trigger("re_import");
+			});
+		}
+	},
+	download_files: function (frm) {
+		window.open(
+			`/api/method/banking.ebics.doctype.ebics_request.ebics_request.download_files?name=${encodeURIComponent(
+				frm.doc.name
+			)}`
+		);
+	},
+	re_import: async function (frm) {
+		try {
+			await confirm_re_import(frm.doc.ebics_user);
+		} catch {
+			frappe.show_alert({
+				message: __("Re-import cancelled."),
+				indicator: "red",
+			});
+			return;
+		}
+
+		frm
+			.call({
+				doc: frm.doc,
+				method: "re_import",
+				freeze: true,
+				freeze_message: __("Re-importing EBICS transactions ..."),
+			})
+			.then(() => {
+				frappe.show_alert({
+					message: __("EBICS transactions re-imported successfully."),
+					indicator: "green",
+				});
+			})
+			.catch(() => {
+				frappe.show_alert({
+					message: __("Failed to re-import EBICS transactions."),
+					indicator: "red",
+				});
+			});
+	},
+>>>>>>> 29351ff (feat: re-import ebics request (#320))
 });
+
+function confirm_re_import(ebics_user) {
+	return new Promise((resolve, reject) => {
+		frappe.db
+			.get_value("EBICS User", ebics_user, "download_batch_transactions")
+			.then((r) => {
+				if (r?.message?.download_batch_transactions) {
+					frappe.confirm(
+						__(
+							"<b>EBICS User</b> '{0}' has <i>Download Batch Transactions</i> enabled. Batch details are stored in separate <b>EBICS Requests</b>, so this re-import might be incomplete. Do you want to continue?",
+							[ebics_user]
+						),
+						() => resolve(),
+						() => reject()
+					);
+				} else {
+					resolve();
+				}
+			})
+			.catch(() => {
+				reject();
+			});
+	});
+}
--- banking/ebics/doctype/ebics_request/ebics_request.py
+++ banking/ebics/doctype/ebics_request/ebics_request.py
@@ -1,9 +1,94 @@
 # Copyright (c) 2025, ALYF GmbH and contributors
 # For license information, please see license.txt
 
+<<<<<<< HEAD
 # import frappe
+=======
+import json
+
+import frappe
+from frappe import _
+>>>>>>> 29351ff (feat: re-import ebics request (#320))
 from frappe.model.document import Document
+
+from banking.ebics.utils import import_ebics_json, register_fintech
 
 
 class EBICSRequest(Document):
+<<<<<<< HEAD
 	pass
+=======
+	@frappe.whitelist()
+	def re_import(self):
+		"""Re-import the EBICS transactions from the response."""
+		ebics_user = frappe.get_doc("EBICS User", self.ebics_user)
+		try:
+			data = json.loads(self.response)
+		except (json.JSONDecodeError, TypeError):
+			frappe.throw(_("Invalid data for re-import."))
+
+		if not isinstance(data, dict):
+			frappe.throw(_("Invalid data for re-import."))
+
+		register_fintech(needs_license_key=True)
+		import_ebics_json(ebics_user, data)
+
+
+def sanitize_filename(filename: str) -> str:
+	"""
+	Sanitize a filename for use in zip files by removing path components.
+
+	Args:
+		filename: The original filename from user input
+
+	Returns:
+		A sanitized filename that's safe to use
+	"""
+	from os.path import basename
+
+	if not filename or not isinstance(filename, str):
+		return "unnamed_file"
+
+	# Remove any path components to get just the basename
+	filename = basename(filename).strip()
+
+	# Fallback if filename becomes empty after sanitization
+	return filename or "unnamed_file"
+
+
+@frappe.whitelist()
+def download_files(name: str):
+	"""
+	Download the files from the EBICS Request as a zip file.
+
+	Args:
+		name: The name of the EBICS Request.
+	"""
+	import json
+	import zipfile
+	from io import BytesIO
+
+	from frappe import _
+
+	doc: EBICSRequest = frappe.get_doc("EBICS Request", name)
+	doc.check_permission()
+
+	try:
+		data = json.loads(doc.response)
+	except (json.JSONDecodeError, TypeError):
+		frappe.throw(_("No data available for download."))
+
+	if not isinstance(data, dict):
+		frappe.throw(_("Invalid data available for download."))
+
+	zip_buffer = BytesIO()
+
+	with zipfile.ZipFile(zip_buffer, mode="w", compression=zipfile.ZIP_DEFLATED) as zip_file:
+		for file_name, file_data in data.items():
+			safe_filename = sanitize_filename(file_name)
+			zip_file.writestr(safe_filename, file_data)
+
+	frappe.response["filecontent"] = zip_buffer.getvalue()
+	frappe.response["filename"] = f"{name}.zip"
+	frappe.response["type"] = "binary"
+>>>>>>> 29351ff (feat: re-import ebics request (#320))
