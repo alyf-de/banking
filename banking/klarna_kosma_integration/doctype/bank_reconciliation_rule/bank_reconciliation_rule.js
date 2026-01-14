276ba763a7ae7a389f5f8230cf306cfe3c2caa03
barredterra <14891507+barredterra@users.noreply.github.com>
2026-01-14 02:07:44 +0100

    refactor(Bank Reconciliation Rule): render_bt_filters

--- banking/klarna_kosma_integration/doctype/bank_reconciliation_rule/bank_reconciliation_rule.js
+++ banking/klarna_kosma_integration/doctype/bank_reconciliation_rule/bank_reconciliation_rule.js
@@ -3,7 +3,7 @@
 
 frappe.ui.form.on("Bank Reconciliation Rule", {
 	onload: function (frm) {
-		render_bt_filters(frm);
+		frm.trigger("render_bt_filters");
 	},
 	refresh(frm) {
 		frm.trigger("set_target_account_query");
@@ -38,26 +38,25 @@ frappe.ui.form.on("Bank Reconciliation Rule", {
 			},
 		}));
 	},
-});
+	render_bt_filters(frm) {
+		const parent = frm.fields_dict.filter_area.$wrapper;
+		parent.empty();
 
-let render_bt_filters = function (frm) {
-	const parent = frm.fields_dict.filter_area.$wrapper;
-	parent.empty();
+		const filters =
+			frm.doc.filters && frm.doc.filters !== "[]"
+				? JSON.parse(frm.doc.filters)
+				: [];
 
-	const filters =
-		frm.doc.filters && frm.doc.filters !== "[]"
-			? JSON.parse(frm.doc.filters)
-			: [];
+		frappe.model.with_doctype("Bank Transaction", () => {
+			const filter_group = new frappe.ui.FilterGroup({
+				parent: parent,
+				doctype: "Bank Transaction",
+				on_change: () => {
+					frm.set_value("filters", JSON.stringify(filter_group.get_filters()));
+				},
+			});
 
-	frappe.model.with_doctype("Bank Transaction", () => {
-		const filter_group = new frappe.ui.FilterGroup({
-			parent: parent,
-			doctype: "Bank Transaction",
-			on_change: () => {
-				frm.set_value("filters", JSON.stringify(filter_group.get_filters()));
-			},
+			filter_group.add_filters_to_filter_group(filters);
 		});
-
-		filter_group.add_filters_to_filter_group(filters);
-	});
-};
+	},
+});
