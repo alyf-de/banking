7f34e571a86cfeb5aae09d691941b0ed1f7df957
0xD0M1M0 <76812428+0xD0M1M0@users.noreply.github.com>
2026-02-06 18:38:48 +0100

    feat: move currency check after insert (#329)
    Co-authored-by: Raffael Meyer <14891507+barredterra@users.noreply.github.com>

--- banking/klarna_kosma_integration/doctype/bank_reconciliation_tool_beta/bank_reconciliation_tool_beta.py
+++ banking/klarna_kosma_integration/doctype/bank_reconciliation_tool_beta/bank_reconciliation_tool_beta.py
@@ -150,13 +150,6 @@ def create_journal_entry_bts(
 			_("Party Type and Party is required for Receivable / Payable account {0}").format(second_account)
 		)
 
-	if second_account_currency != bank_account_currency:
-		frappe.throw(
-			_(
-				"The currency of the second account ({0} : {1}) must be the same as of the bank account ({2} : {3})"
-			).format(second_account, second_account_currency, bank_gl_account, bank_account_currency)
-		)
-
 	journal_entry = frappe.new_doc("Journal Entry")
 	journal_entry.update(
 		{
@@ -199,6 +192,15 @@ def create_journal_entry_bts(
 
 	if allow_edit:
 		return journal_entry  # Return saved document
+
+	# This check happens here because the user should be able to make
+	# multicurrency entries when they edit the Journal Entry manually (`allow_edit` is True).
+	if second_account_currency != bank_account_currency:
+		frappe.throw(
+			_(
+				"The currency of the second account ({0} : {1}) must be the same as of the bank account ({2} : {3})"
+			).format(second_account, second_account_currency, bank_gl_account, bank_account_currency)
+		)
 
 	journal_entry.submit()
 
