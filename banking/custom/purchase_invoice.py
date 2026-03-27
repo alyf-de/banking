f50cf307afb9bdc417cd830c99f8089c5e2cdfef
barredterra <14891507+barredterra@users.noreply.github.com>
2026-03-27 13:07:31 +0100

    fix: add type hint

--- banking/custom/purchase_invoice.py
+++ banking/custom/purchase_invoice.py
@@ -12,10 +12,11 @@ if TYPE_CHECKING:
 	from erpnext.accounts.doctype.purchase_invoice.purchase_invoice import PurchaseInvoice
 
 	from banking.ebics.doctype.sepa_payment.sepa_payment import SEPAPayment
+	from banking.ebics.doctype.sepa_payment_order.sepa_payment_order import SEPAPaymentOrder
 
 
 @frappe.whitelist()
-def make_sepa_payment_order(source_name: str, target_doc=None):
+def make_sepa_payment_order(source_name: str, target_doc: "SEPAPaymentOrder | None" = None):
 	def set_missing_values(source, target):
 		if not target.bank_account:
 			bank_account = frappe.db.get_value(
