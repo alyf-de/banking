74bb1efcfd3969e21c65fa2aa7d8e1b92495576d
barredterra <14891507+barredterra@users.noreply.github.com>
2026-04-21 00:47:09 +0200

    test: install payments app
    Needed for transitive test dependencies

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -34,6 +34,7 @@ bench get-app banking "${GITHUB_WORKSPACE}"
 
 bench start &> bench_start.log &
 bench new-site --db-root-password root --admin-password admin test_site --install-app erpnext
+bench --site test_site install-app payments
 bench --site test_site install-app hrms
 bench --site test_site install-app banking
 bench setup requirements --dev
