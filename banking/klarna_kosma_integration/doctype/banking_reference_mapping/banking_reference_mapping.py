2eaef635413d298a8f0ccf62d20b7c3618266b54
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2026-05-21 04:46:32 +0200

    chore: resave doctypes with v16 metadata (#371)

--- banking/klarna_kosma_integration/doctype/banking_reference_mapping/banking_reference_mapping.py
+++ banking/klarna_kosma_integration/doctype/banking_reference_mapping/banking_reference_mapping.py
@@ -6,4 +6,19 @@ from frappe.model.document import Document
 
 
 class BankingReferenceMapping(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		document_type: DF.Literal["Sales Invoice", "Purchase Invoice", "Expense Claim"]
+		field_name: DF.Literal[None]
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+	# end: auto-generated types
+
 	pass
