d5f220fa0c76e20f55a3ad2d048637a8233a548b
barredterra <14891507+barredterra@users.noreply.github.com>
2026-05-22 14:21:54 +0200

    fix: insert_custom_records, uninstall
    - copy dict before modifying
    - pop _required_apps for uninstall query
    - use standard delete_custom_fields
    - use standard delete_property_setter

--- banking/install.py
+++ banking/install.py
@@ -31,16 +31,17 @@ def make_property_setters():
 
 def insert_custom_records():
 	for custom_record in frappe.get_hooks("alyf_banking_custom_records"):
-		if required_apps := custom_record.pop("_required_apps", None):
+		custom_record_copy = custom_record.copy()
+		if required_apps := custom_record_copy.pop("_required_apps", None):
 			installed_apps = frappe.get_installed_apps()
 			if any(app not in installed_apps for app in required_apps):
 				continue
 
-		filters = custom_record.copy()
+		filters = custom_record_copy.copy()
 		# Clean up filters. They need to be a plain dict without nested dicts or lists.
-		for key, value in custom_record.items():
+		for key, value in custom_record_copy.items():
 			if isinstance(value, list | dict):
 				del filters[key]
 
 		if not frappe.db.exists(filters):
-			frappe.get_doc(custom_record).insert(ignore_if_duplicate=True)
+			frappe.get_doc(custom_record_copy).insert(ignore_if_duplicate=True)
--- banking/uninstall.py
+++ banking/uninstall.py
@@ -11,42 +11,53 @@ def before_uninstall():
 
 def remove_custom_records():
 	print("* removing custom records...")
+	doctypes_to_clear = set()
 	for record in frappe.get_hooks("alyf_banking_custom_records"):
-		doctype = record.pop("doctype")
-		frappe.db.delete(doctype, record)
+		record_copy = record.copy()
+		record_copy.pop("_required_apps", None)
+		doctype = record_copy.pop("doctype")
+
+		filters = record_copy.copy()
+		# Clean up filters. They need to be a plain dict without nested dicts or lists.
+		for key, value in record_copy.items():
+			if isinstance(value, list | dict):
+				del filters[key]
+
+		frappe.db.delete(doctype, filters)
+		if parent := filters.get("parent"):
+			doctypes_to_clear.add(parent)
+
+	for parent in doctypes_to_clear:
+		frappe.clear_cache(doctype=parent)
 
 
 def remove_custom_fields():
 	print("* removing custom fields...")
+	from frappe.custom.doctype.custom_field.custom_field import delete_custom_fields
+
+	custom_fields_to_delete = {}
 	for doctypes, custom_fields in get_custom_fields().items():
 		if isinstance(doctypes, str):
 			doctypes = (doctypes,)
 
 		for doctype in doctypes:
-			for cf in custom_fields:
-				frappe.db.delete(
-					"Custom Field",
-					{
-						"dt": doctype,
-						"fieldname": cf.get("fieldname"),
-					},
-				)
+			custom_fields_to_delete[doctype] = custom_fields
+
+	delete_custom_fields(custom_fields_to_delete)
 
 
 def remove_property_setters():
 	print("* removing property setters...")
+	from frappe.custom.doctype.property_setter.property_setter import delete_property_setter
+
 	for doctypes, property_setters in frappe.get_hooks("alyf_banking_property_setters", {}).items():
 		if isinstance(doctypes, str):
 			doctypes = (doctypes,)
 
 		for doctype in doctypes:
 			for property_setter in property_setters:
-				frappe.db.delete(
-					"Property Setter",
-					{
-						"doc_type": doctype,
-						"field_name": property_setter.get("fieldname"),
-						"property": property_setter.get("property"),
-						"value": property_setter.get("value"),
-					},
+				delete_property_setter(
+					doctype,
+					property=property_setter.get("property"),
+					field_name=property_setter.get("fieldname"),
 				)
